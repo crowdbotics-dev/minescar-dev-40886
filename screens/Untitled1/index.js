e0b196bce61b70e64cddfd8136ef36ce8423737b
scartag <tag@crowdbotics.com>
2022-11-02 16:35:10 +0000

    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled1/index.js
@@ -0,0 +1,29 @@
+import { Text } from "react-native";
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled1 = () => {
+  return <View style={_styles.rBdaVrip}>
+      <Text style={_styles.DsyBRJAS}>Lorem ipsum…</Text></View>;
+};
+
+export default Untitled1;
+
+const _styles = StyleSheet.create({
+  rBdaVrip: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  },
+  DsyBRJAS: {
+    left: 127,
+    top: 191,
+    position: "absolute",
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  }
+});
