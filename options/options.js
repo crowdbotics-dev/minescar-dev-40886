2080d2c97c8550df3f533a421996aef8f781c186
scartag <tag@crowdbotics.com>
2022-11-08 17:27:48 +0000

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,9 @@
+const styles = StyleSheet.create({});
 export const globalOptions = {
-  name: "minescar_dev_40886",
-  url: "https://minescar_dev_40886.botics.co",
-  api: "https://minescar_dev_40886.botics.co/api/v1"
+  "name": "minescar_dev_40886",
+  "url": "https://minescar_dev_40886.botics.co",
+  "api": "https://minescar_dev_40886.botics.co/api/v1"
 };
 export const modulesOptions = {
-  "@modules/app-menu": {
-    "copy": "Routes available!"
-  }
+  "@modules/app-menu": {}
 };
